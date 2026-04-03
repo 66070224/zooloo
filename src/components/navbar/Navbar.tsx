a5c8c0ead390682247a75a5fbdcd9bba50553ff6
IT21Neo <66070320@kmitl.ac.th>
2026-04-03 19:03:25 +0700

    แก้ navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import NavbarLink from "./NavbarLink";
 export default function Navbar({ index }: { index: number }) {
   return (
     <nav
-      className={`${index === 0 && "bg-yale-blue"} ${index === 1 && "bg-light-green"} ${index === 2 && "bg-cerulean"} text-white flex justify-center items-center gap-5 p-5 m-4 text-xs h-10 justify-self-center rounded-full fixed top-0 z-50`}
+      className={`${index === 0 && "bg-yale-blue"} ${index === 1 && "bg-amber-600"} ${index === 2 && "bg-cerulean"} text-white flex justify-center items-center gap-5 p-5 m-4 text-xs h-10 justify-self-center rounded-full fixed top-0 z-50`}
     >
       <Link href="/" className="p-1 text-base font-bold text-gray-800">
         ZOO<span className="text-yellow-500">LOO</span>
